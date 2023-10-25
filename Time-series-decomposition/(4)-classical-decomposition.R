b286ded0e7ff3e2d5650a627c8ba1d40eab145f9
Ian Kanda Bernucci <84795478+ianbernucci@users.noreply.github.com>
2023-10-25 15:13:00 -0300

    Create (4)-classical-decomposition.R

---
+++ Time-series-decomposition/(4)-classical-decomposition.R
@@ -0,0 +1,10 @@
+# CLASSICAL DECOMPOSITION
+
+us_retail_employment |>
+  model(
+    classical_decomposition(Employed, type = "additive")
+  ) |>
+  components() |>
+  autoplot() +
+  labs(title = "Classical additive decomposition of total
+                  US retail employment")
